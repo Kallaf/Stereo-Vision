2e065c94c069fa2e9d2f2707ab9f66d100687291
kallaf <kalaf1997@yahoo.com>
2019-12-14 14:57:24 +0200

    Adding block matching

---
+++ Stereo.py
@@ -0,0 +1,58 @@
+import numpy as np
+import math
+from PIL import Image
+
+class Stereo:
+    def __init__(self,leftI,rightI,disparity_range_):
+        self.disparityRange = disparity_range_
+
+        # Load in both images, assumed to be RGBA 8bit per channel images
+        self.leftImage = Image.open(leftI).convert('L')
+        self.rightImage = Image.open(rightI).convert('L')
+        self.left = np.asarray(self.leftImage)
+        self.right = np.asarray(self.rightImage)
+        
+    def match(self,kernel):
+        print("calculating disparity matrix for w = ",kernel)
+        w, h = self.leftImage.size  # assume that both images are same size   
+        
+        # Depth (or disparity) map
+        depth = np.zeros((w, h), np.uint8)
+        depth.shape = h, w
+        
+        kernel_half = int(kernel / 2)    
+        offset_adjust = 255 / self.disparityRange  # this is used to map depth map output to 0-255 range
+        
+        for y in range(kernel_half, h - kernel_half):      
+            print(".", end="", flush=True)  # let the user know that something is happening (slowly!)
+            
+            for x in range(kernel_half, w - kernel_half):
+                best_offset = 0
+                prev_ssd = 65534
+                
+                for offset in range(self.disparityRange):               
+                    ssd = 0
+                    ssd_temp = 0                            
+                    
+                    # v and u are the x,y of our local window search, used to ensure a good 
+                    # match- going by the squared differences of two pixels alone is insufficient, 
+                    # we want to go by the squared differences of the neighbouring pixels too
+                    for v in range(-kernel_half, kernel_half):
+                        for u in range(-kernel_half, kernel_half):
+                            # iteratively sum the sum of squared differences value for this block
+                            # left[] and right[] are arrays of uint8, so converting them to int saves
+                            # potential overflow, and executes a lot faster 
+                            ssd_temp = int(self.left[y+v, x+u]) - int(self.right[y+v, (x+u) - offset])  
+                            ssd += ssd_temp * ssd_temp              
+                    
+                    # if this value is smaller than the previous ssd at this block
+                    # then it's theoretically a closer match. Store this value against
+                    # this block..
+                    if ssd < prev_ssd:
+                        prev_ssd = ssd
+                        best_offset = offset
+                                
+                # set depth output for this x,y location to the best match
+                depth[y, x] = best_offset * offset_adjust
+        print()
+        return depth
--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from PIL import Image 
+#!/usr/bin/env python
+# Python 2/3 compatibility
+import numpy as np
+import cv2
+from Stereo import Stereo
+from PIL import Image
 
-images_folder = "Images/Example1"
+images_folder = "Images/Example2"
 
-left_img = Image.open(images_folder+'/left.png') 
-right_img = Image.open(images_folder+'/right.png')  
+stereo = Stereo(images_folder+'/left.png', images_folder+'/right.png', 8)
 
-left_img.show()
-right_img.show()
+kernels = [1,5,9]
+for kernel in kernels:
+    depth = stereo.match(kernel)  # 6x6 local search kernel, 30 pixel search range
+    # Convert to PIL and save it
+    Image.fromarray(depth).save(images_folder+'/depth_'+str(kernel)+'.png')
